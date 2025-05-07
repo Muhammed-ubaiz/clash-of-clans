e0aa1d8fc83197fdcc949be5bc525191b98f44e7
ubais <muhammedubais01@gmail.com>
2025-05-07 12:17:50 +0530

    build is done

--- src/component/LastSection.jsx
+++ src/component/LastSection.jsx
@@ -7,7 +7,7 @@ function LastSection() {
                 <h1 className="text-white font-bold text-center text-4xl pt-20 md:pt-2">INTERESTED IN WORKING WITH US?</h1>
                 <button type="submit" className="bg-white text-black font-bold py-3 px-4 rounded mt-14 md:mt-4">SEE ALL POSITIONS</button>
                 </div>
-                <div className="flex flex-row mt-14 justify-center md">
+                <div className="flex flex-row mt-14 justify-center md">   
                 <img src="./images/mini_pekka_fl.9f294fd5.png" className="w-[38%] md:w-[50%] md:mt-[-22%]" alt="" />
             </div>
         </div>
