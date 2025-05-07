ffad3f4b1e879f0abda28217222065159af9d1c8
ubais <muhammedubais01@gmail.com>
2025-05-07 13:56:57 +0530

    image added

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -3,7 +3,7 @@ function Nav() {
     <>
       <div className=" flex justify-center items-center w-full">
         <nav className="bg-black w-[80%] h-[11vh] top-[0%] hidden md:flex text-center items-center gap-x-8 fixed justify-between">
-        <img src="./images/supercell_logo.50e3c1ff.webp" className="w-[8%] p-4" alt="" />
+        <img src="https://res.cloudinary.com/danntnn1q/image/upload/v1746601585/supercell_logo.50e3c1ff_gnyk4t.webp" className="w-[8%] p-4" alt="" />
         <ul className="flex flex-row text-white font-bold gap-x-8 pr-[40%]">
           <a href="Games"><li className="text-zinc-400 hover:text-white">Games</li></a>         
           <a href="Careers"><li className="text-zinc-400 hover:text-white">Careers</li></a>
@@ -13,7 +13,7 @@ function Nav() {
         <div className="flex flex-row gap-x-8 pr-[5%]">
           <img src="https://res.cloudinary.com/danntnn1q/image/upload/v1746601585/supercell_make_grey.964b2d8d_dwbttc.svg" className="w-6" alt="" />
           <img src="https://res.cloudinary.com/danntnn1q/image/upload/v1746601585/supercell_id_grey.62affa0b_uyk8i6.svg" className="w-8" alt="" /> 
-          <img src="./images/supercell_id_grey.62affa0b.svg" className="w-6" alt="" />
+          <img src="https://res.cloudinary.com/danntnn1q/image/upload/v1746601584/supercell_creators_grey.f699541d_oewbzu.svg" className="w-6" alt="" />
         </div>
         </nav>
       </div>
